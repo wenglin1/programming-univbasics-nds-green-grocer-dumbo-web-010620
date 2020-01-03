d4d1a4c2428d0fca7f1b2c958c3f28729b19ec76
weng lin <wenglin@buffalo.edu>
2020-01-03 11:54:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,8 +33,8 @@ end
 def apply_coupons(cart, coupons)
    index = 0
    while index < coupons.length
-     cart_item = find_item_by_name_in_collection(coupons[index][:items], cart)
-     couponed_item_name = "#{coupons[index][:items]} W/COUPON"
+     cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
+     couponed_item_name = "#{coupons[index][:item]} W/COUPON"
      cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
      if cart_item && cart_item[:count] >= coupons[index][:num]
        if cart_item_with_coupon
