32254035a2d569522fe4f46492cd0fbf120289ef
weng lin <wenglin@buffalo.edu>
2020-01-03 11:59:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,33 +31,33 @@ end
 
 
 def apply_coupons(cart, coupons)
-   index = 0
-   while index < coupons.length
-     cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
-     couponed_item_name = "#{coupons[index][:item]} W/COUPON"
-     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-     if cart_item && cart_item[:count] >= coupons[index][:num]
-       if cart_item_with_coupon
-         cart_item_with_coupon[:count] += coupons[index][:num]
-         cart_item[:count] -= coupons[index][:num]
-       else
-         cart_item_with_coupon = {
-           :item => couponed_item_name,
-           :price => coupons[index][:cost] / coupons[index][:num],
-           :count => coupons[index][:num],
-           :clearance => cart_item[:clearance]
-         }
-         cart << cart_item_with_coupon
-         cart_item[:count] -= coupons[index][:num]
-       end
-     end
-     index += 1
-   end
-   cart
+  index = 0
+  while index < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
+    couponed_item_name = "#{coupons[index][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[index][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[index][:num]
+        cart_item[:count] -= coupons[index][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[index][:cost] / coupons[index][:num],
+          :count => coupons[index][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[index][:num]
+      end
+    end
+    index += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-
+  new_
 end
 
 def checkout(cart, coupons)
