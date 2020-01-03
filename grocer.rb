abb51e4ed4a602a46062ea412fd9fc487fa295c6
weng lin <wenglin@buffalo.edu>
2020-01-03 12:12:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -69,7 +69,7 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidate_cartd, coupons)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   total = 0
   index = 0
